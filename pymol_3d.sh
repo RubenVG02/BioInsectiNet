04195cfb9df950844ee90696a0f58c7bb9f6c090
RubenVG02 <rubenvillegas2@gmail.com>
2023-05-24 13:21:24 +0200

    3d representation

---
+++ pymol_3d.sh
@@ -0,0 +1,7 @@
+
+cd /Users/rubenvg/Desktop/antiinsecticides/Fungic_insecticides/
+open -a PyMOL
+cmd.load('molecule_sdf.sdf')
+cmd.save('molecule_pdb.stl')
+
+#load Users/rubenvg/Desktop/antiinsecticides/Fungic_insecticides/molecule_sdf.sdf
