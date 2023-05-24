19eba62b51be6efe13d85476eb4e25120eb28707
RubenVG02 <rubenvillegas2@gmail.com>
2023-05-25 00:31:09 +0200

    script for pymol

--- file_for_pymol.py
+++ file_for_pymol.py
@@ -1,5 +1,5 @@
 ## This script will transform our sdf file into a stl file using pymol
-## We will use the following command: pymol -c file_for_pymol.py
+## We will use this script using the pymol terminal, and the stl file will be saved in the same folder as the sdf file
 
 cmd.load('molecule_sdf.sdf')
 cmd.save('molecule_stl.stl')
