74e3896a3e006dd7fd7fea74aa709cb40cd61206
Rubén VG <rubenvillegas2@gmail.com>
2025-07-11 14:13:32 +0200

    Refactor check_affinity.py to initialize models and tokenizers outside of predict_affinity function for improved clarity and efficiency

--- src/check_affinity.py
+++ src/check_affinity.py
@@ -5,6 +5,13 @@ from transformers import AutoTokenizer, AutoModel
 
 from scripts_with_other_functions.get_hyperparams_db import get_best_trial
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+tokenizer_fasta = AutoTokenizer.from_pretrained("facebook/esm2_t12_35M_UR50D",trust_remote_code=True)
+model_fasta = AutoModel.from_pretrained("facebook/esm2_t12_35M_UR50D", torch_dtype=torch.float16).to(device)
+tokenizer_smiles = AutoTokenizer.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k")
+model_smiles = AutoModel.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k").to(device)
+
 def predict_affinity(smile, fasta, path_model, best_trial):
     model = CombinedModel(hidden_dim=best_trial["params"]["hidden_dim"],
                           num_layers=best_trial["params"]["num_layers"],
@@ -28,12 +35,8 @@ def predict_affinity(smile, fasta, path_model, best_trial):
     fasta_emb = (fasta_outputs.last_hidden_state * attention_mask_fasta).sum(1) / attention_mask_fasta.sum(1).clamp(min=1e-9)
     fasta_emb = fasta_emb.float()
 
-    print("chem_emb shape:", chem_emb.shape)
-    print("fasta_emb shape:", fasta_emb.shape)
-
     with torch.no_grad():
         output_tensor = model(chem_emb, fasta_emb)
-        print("Raw model output:", output_tensor)
         affinity = output_tensor.squeeze().item()
     
 
@@ -46,13 +49,6 @@ if __name__ == "__main__":
     path_model = r"models\checkpoints\cnn_affinity\trial_1_loss_0.1974.pth"
 
     best_trial = get_best_trial("models/cnn_affinity.db", study_name="cnn_affinity")
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    tokenizer_fasta = AutoTokenizer.from_pretrained("facebook/esm2_t12_35M_UR50D",trust_remote_code=True)
-    model_fasta = AutoModel.from_pretrained("facebook/esm2_t12_35M_UR50D", torch_dtype=torch.float16).to(device)
-    tokenizer_smiles = AutoTokenizer.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k")
-    model_smiles = AutoModel.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k").to(device)
 
 
     raw = predict_affinity(smile, fasta, path_model, best_trial=best_trial)
