5c7f21b61d0f4b8a090dcf7cb9e20ee9f1ee9fd3
Rubén VG <rubenvillegas2@gmail.com>
2025-07-31 16:29:03 +0200

    Add logger utility functions to __init__.py for improved logging capabilities

---
+++ src/utils/__init__.py
@@ -0,0 +1,3 @@
+from .logger import get_logger, log_info, log_warning, log_error, log_success, log_debug, set_log_file
+
+__all__ = ['get_logger', 'log_info', 'log_warning', 'log_error', 'log_success', 'log_debug', 'set_log_file']
